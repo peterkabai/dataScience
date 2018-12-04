b43bd63853424844832c8f91a384e01f749f562b
Peter <pkabai@csumb.edu>
2018-12-03 21:59:12 -0800

    Adds file on vectors

---
+++ vectors.R
@@ -0,0 +1,37 @@
+# Creates a vector of strings
+v1 = c("foo", "bar", "baz")
+
+# Gets the first value
+# Note that R is not 0 indexed
+v1[1]
+
+# Creates a vector of numbers
+v2 = c(1, 2, 3)
+
+# Concatinate vectors
+# Note that `paste` concatinates with spaces and `paste0` without spaces
+paste(v1, v2)
+paste0(v1, v2)
+
+# Generate 10 random numbers from 1 to 100
+# Note the `FALSE` argument means without replacement
+# The default is `TRUE` meaning there can be duplicates
+x = sample(1:100, 10, FALSE)
+print(x)
+
+# The mean can be found in multiple ways
+mean(x)
+sum(x)/length(x)
+
+# The trim argument is used to cut off a fraction of observations before finding the mean
+# By default it's 0
+mean(c(1,2,3,4,5,6,7,8,9,10), 0.2)
+
+# The `na.rm` argument can remove `NA` values before computing
+mean(c(1,3,NA,NA,NA))
+mean(c(1,3,NA,NA,NA), na.rm=TRUE)   
+
+# Here are some other functions that can be used on vectors
+v = c(1,2,3,4,5,6,7,8,9,10)
+min(v)
+max(v)
