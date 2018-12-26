35118fac8eafa79e0c1750cd55fa34acddcfb19d
Peter <pkabai@csumb.edu>
2018-12-25 19:56:51 -0800

    Removes code outside the function

--- splitData.R
+++ splitData.R
@@ -4,14 +4,4 @@ split = function(dat, frac) {
   train = dat[indices,]
   test = dat[-indices,]
   return(list(train, test))
-} 
-
-# create data frame using the two vectors
-v1 = c("one", "two", "three", "four")
-v2 = c(1:4)
-dat = data.frame("word"=v1, "numbers"=v2, stringsAsFactors=FALSE)
-
-# split the data
-splitData = split(dat, 0.75)
-splitData[[1]]
-splitData[[2]]
+}
