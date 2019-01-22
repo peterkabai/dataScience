88dd3e2b49cf8ab026f4919e6c240f581231c828
Peter <pkabai@csumb.edu>
2019-01-22 13:38:59 -0800

    Adds lab 2 of the linear regression lab

--- linearRegression.R
+++ linearRegression.R
@@ -28,3 +28,41 @@ abline(0,0,col="red")
 # examine the output of summary(fit) and plot(fit)
 summary(fit)
 plot(fit)
+
+# pick two features to use as predictor variable, to predict 'prp'
+fit = lm(prp ~ mmin + chmax, data=machine)
+
+# run summary(fit) to get some information on your model
+summary(fit)
+
+# print the coefficients of your model
+fit$coefficients
+
+# print the adjusted R-squared value you obtained
+summary(fit)$adj.r.squared
+
+# produce a scatterplot in which the predicted prp values are shown on the x axis and 
+# the actual prp values are shown on the y axis.
+prediction = predict(fit, dat)
+plot(prediction, dat$prp)
+
+# run plot(fit) and look at the Residuals vs Fitted plot and the Normal Q-Q plot.
+plot(fit)
+
+# analyse for normal distribution, and constant residual variance
+residuals = resid(fit)
+plot(density(residuals))
+
+# now use two different predictors
+fit = lm(prp ~ myct + cach, data=dat)
+residuals = resid(fit)
+prediction = predict(fit, dat)
+plot(prediction, dat$prp)
+plot(density(residuals))
+
+# now use all predictors
+fit = lm(prp ~ ., data=dat)
+residuals = resid(fit)
+prediction = predict(fit, dat)
+plot(prediction, dat$prp)
+plot(density(residuals))
